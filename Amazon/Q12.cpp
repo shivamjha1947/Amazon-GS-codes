6e355ce36b22aa6a213a1217381a6c7aa66a02e2
shivam jha <shivam36_soe@jnu.ac.in>
2022-01-10 12:22:37 +0530

    Create Q12.cpp

---
+++ Amazon/Q12.cpp
@@ -0,0 +1,11 @@
+string colName(long long int n){
+    string res;
+    char tmp;
+    while (n){
+        n -= 1;
+        tmp = 'A' + n % 26;
+        res = tmp + res;
+        n /= 26;
+    }
+    return res;
+}
